c7fb66a8efc290c82ad139d388edf7285b4b17ed
Tom2011Tom <84624601+Tom2011Tom@users.noreply.github.com>
2021-08-05 14:32:54 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,197 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # #
+            . . . . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . # #
+            . . # # #
+            . . . # #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . # # # #
+            . . # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # # . #
+            # # # # #
+            . # # . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # # . # .
+            # # # # .
+            # # . # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . # . .
+            # # # . .
+            # . # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            # # . . .
+            . # . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 7; index++) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # . # #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            . . . . .
+            . # . . .
+            # . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . # . .
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            # # # # #
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            . # # # .
+            # # # # #
+            # . . . #
+            `)
+        basic.showLeds(`
+            # # # # #
+            . # # # .
+            # # # # #
+            # . . . #
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
